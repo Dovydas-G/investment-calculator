faaf9e86c51b101e13ad21bfd5c07096debe3617
Dovydas-G <146329425+Dovydas-G@users.noreply.github.com>
2024-08-22 11:56:24 +0300

    Update

--- src/components/UserInput.jsx
+++ src/components/UserInput.jsx
@@ -1,13 +1,5 @@
 export default function UserInput ({ onChange, userInput }) {
-    
-    
 
-    // const result = calculateInvestmentResults(+(userInput.initialInvestment), +(userInput.annualInvestment), +(userInput.expectedReturn), +(userInput.duration))
-    
-    // for (const item of result) {
-    //     console.log(formatter.format(item.valueEndOfYear))
-    // }
-    
     return (
         <>
             <section id="user-input" action="">
@@ -32,28 +24,6 @@ export default function UserInput ({ onChange, userInput }) {
                     </p>
                 </div>
             </section>
-            {/* <table id="result">
-                <thead id="result thead">
-                    <tr>
-                        <th>Year</th>
-                        <th>Investment Value</th>
-                        <th>Interest(Year)</th>
-                        <th>Total interest</th>
-                        <th>Invested Capital</th>
-                    </tr>
-                </thead>
-                <tbody id="result tbody">
-                   {result.map((row, rowIndex) => (
-                        <tr key={rowIndex}>
-                            <td>{row.year}</td>
-                            <td>{formatter.format(row.valueEndOfYear)}</td>
-                            <td>{formatter.format(row.interest)}</td>
-                            <td>{formatter.format(row.valueEndOfYear - (+initialInvestment + (row.annualInvestment * row.year)))}</td>
-                            <td>{formatter.format(+initialInvestment + (row.annualInvestment * row.year))}</td>
-                        </tr>
-                   ))}
-                </tbody>
-            </table> */}
         </>
     )
 }
